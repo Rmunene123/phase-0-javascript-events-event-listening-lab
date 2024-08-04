d3da193c80c3a5624394c399264861d9bf103fbc
Rmunene123 <rosewanmun@gmail.com>
2024-08-05 01:21:28 +0300

    added event listeners

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 function addingEventListener() {
-}
+        // Select the button element by its ID
+        const input = document.getElementById('button');
+        
+        // Add a click event listener to the button
+        input.addEventListener('click', clickAlert);
+    }
+    
+    // Function to display an alert when the button is clicked
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    
+    // Call the function to set up the event listener
+    addingEventListener();
+    
+
+    
